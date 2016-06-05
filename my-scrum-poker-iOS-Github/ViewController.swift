a6581a226fd62222cf81d69ea04db361132220fc
codecamp <codecamp@danielas-macbook-air.local>
2016-06-05 18:26:56 +0200

    Initial Commit

---
+++ my-scrum-poker-iOS-Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-scrum-poker-iOS-Github
+//
+//  Created by codecamp on 05.06.16.
+//  Copyright © 2016 mike-wernli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
